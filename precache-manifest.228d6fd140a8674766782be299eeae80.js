95dd33d8dbca94f55f3a5d4b1bd7bde7a8db940a
Bhartendra Singh Chauhan <65101955+bhartendrachauhan@users.noreply.github.com>
2023-03-10 20:57:59 +0530

    Updates

---
+++ precache-manifest.228d6fd140a8674766782be299eeae80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fa66224d16fb20f91e1",
+    "url": "/bhartendrachauhan/text-utils.git/static/css/main.c34aa2a8.chunk.css"
+  },
+  {
+    "revision": "8fa66224d16fb20f91e1",
+    "url": "/bhartendrachauhan/text-utils.git/static/js/main.0e100cca.chunk.js"
+  },
+  {
+    "revision": "e5639da2ecf8f5b47dec",
+    "url": "/bhartendrachauhan/text-utils.git/static/js/runtime~main.9d24f593.js"
+  },
+  {
+    "revision": "91f98fe5c43339a6e84a",
+    "url": "/bhartendrachauhan/text-utils.git/static/js/2.e3bc30a0.chunk.js"
+  },
+  {
+    "revision": "9e493117a3dfb5238748",
+    "url": "/bhartendrachauhan/text-utils.git/static/js/3.f326d3da.chunk.js"
+  },
+  {
+    "revision": "672c679aa1fcda2f402769c7721a96cf",
+    "url": "/bhartendrachauhan/text-utils.git/index.html"
+  }
+];
